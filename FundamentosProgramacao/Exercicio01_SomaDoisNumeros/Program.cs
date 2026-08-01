7ca76e045648269a96c68051b74759a49567572d
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-01 01:18:03 -0300

    Resolve exercício 01 - soma de dois números

---
+++ FundamentosProgramacao/Exercicio01_SomaDoisNumeros/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Digite o primeiro número: ");
+int primeiroNumero = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+int segundoNumero = int.Parse(Console.ReadLine());
+
+int soma = primeiroNumero + segundoNumero;
+
+Console.WriteLine($"A soma é {soma}.");
