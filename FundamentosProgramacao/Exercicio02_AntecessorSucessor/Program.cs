1d0bc1c880c306368bcdc4447a8e0b6f87f0aad2
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-01 23:29:55 -0300

    Resolve exercício 02 - antecessor e sucessor

--- FundamentosProgramacao/Exercicio02_AntecessorSucessor/Program.cs
+++ FundamentosProgramacao/Exercicio02_AntecessorSucessor/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digit um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+
+int antecessor = numero - 1;
+int sucessor = numero + 1;
+
+Console.WriteLine($"Número antecessor: {antecessor}");
+Console.WriteLine($"Número informado: {numero}");
+Console.WriteLine($"Número sucessor: {sucessor}");
+
