76ec17ebd9f92957cae2749de602497d29df20d7
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-03 02:56:03 -0300

    Resolve exercício 03 - área de um retângulo

---
+++ FundamentosProgramacao/Exercicio03_AreaRetangulo/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Digite a base do retângulo: ");
+double baseRetangulo = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a altura do retângulo: ");
+double alturaRetangulo = double.Parse(Console.ReadLine());
+
+
+double areaRetangulo = baseRetangulo * alturaRetangulo;
+
+double perimetroRetangulo = (baseRetangulo + alturaRetangulo) * 2;
+
+Console.WriteLine($"\nA área do retângulo é: {areaRetangulo}");
+Console.WriteLine($"O perímetro do retângulo é: {perimetroRetangulo}");
+
