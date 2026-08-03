ff35ae7290a84c751f27d09951f88ea14fd9914d
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-03 18:24:33 -0300

    Resolve exercício 04 - conversor de moedas

---
+++ FundamentosProgramacao/Exercicio04_ConversorMoedas/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Write("Digite um valor em reais: ");
+var realDigitado = Console.ReadLine();
+
+if (!decimal.TryParse(realDigitado, out decimal valorReal)) {
+    Console.WriteLine("Valor inválido!");
+    return;
+}
+
+Console.Write("Digite cotação do dólar: ");
+var cotacaoDigitado = Console.ReadLine();
+
+if (!decimal.TryParse(cotacaoDigitado, out decimal cotacaoDolar)) {
+    Console.WriteLine("Valor inválido!");
+    return;
+}
+
+decimal cambio = valorReal / cotacaoDolar;
+
+Console.Write($"\nO valor de {valorReal:C2} equivale a $ {cambio:F2}");
