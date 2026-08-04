e81e788a1500816a91f0a09026520213c9baf7ba
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-03 22:12:54 -0300

    Resolve exercício 05 - calculadora simples

---
+++ FundamentosProgramacao/Exercicio05_CalculadoraSimples/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Digite o primeiro número: ");
+var primeiroDigitado = Console.ReadLine();
+
+if (!int.TryParse(primeiroDigitado, out int primeiroNumero)) {
+    Console.WriteLine("Valor informado é inválido.");
+    return;
+}
+
+Console.Write("Digite o segundo número: ");
+var segundoDigitado = Console.ReadLine();
+
+if (!int.TryParse(segundoDigitado, out int segundoNumero)) {
+    Console.WriteLine("Valor informado é inválido.");
+    return;
+}
+
+if (segundoNumero == 0) {
+    Console.WriteLine("Não é possível dividir por zero.");
+    return;
+}
+
+int soma = primeiroNumero + segundoNumero;
+int subtracao = primeiroNumero - segundoNumero;
+int multiplicacao = primeiroNumero * segundoNumero;
+double divisao = (double)primeiroNumero / segundoNumero;
+
+Console.WriteLine($"\nA soma é {soma}.");
+Console.WriteLine($"A subtracao é {subtracao}.");
+Console.WriteLine($"A multiplicacao é {multiplicacao}.");
+Console.WriteLine($"A divisao é {divisao:F2}.");
