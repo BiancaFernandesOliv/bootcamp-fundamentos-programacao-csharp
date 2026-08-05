2a316cd95e75bb4c5ce551303e5071d6c615cb32
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-04 21:06:12 -0300

    Resolve exercício 14 - maior elemento

---
+++ FundamentosProgramacao/Exercicio14_MaiorElemento/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> numeros = new List<int>();
+
+for (int i = 1; i <= 10; i++) {
+    Console.Write("Digite um número inteiro: ");
+    var numeroDigitado = Console.ReadLine();
+
+    if(!int.TryParse(numeroDigitado, out int numero)) {
+        Console.WriteLine("Valor informado é inválido.");
+        return;
+    }
+
+    numeros.Add(numero);
+}
+
+int maiorNumero = numeros.Max();
+
+Console.WriteLine($"Maior valor: {maiorNumero}");
+
+Console.WriteLine($"Posição em que ele aparece: {numeros.IndexOf(maiorNumero) + 1}");
